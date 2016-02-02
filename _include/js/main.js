770fd943392780325d93f35417c372014e0815af
Onatskaya <Onatskaya.Nadejda@gmail.com>
2016-02-02 14:02:32 +0200

    Change datepicker, add new menu chapters, change colors and backgrounds

--- _include/js/main.js
+++ _include/js/main.js
@@ -77,10 +77,10 @@ BAO.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">BAO - Chinese cuisine</div>', thumb : '', url : ''},
+											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">BAO</div>', thumb : '', url : ''}, 
 											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">BAO</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">BAO</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">BAO</div>', thumb : '', url : ''}  
+											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">BAO</div>', thumb : '', url : ''}
+											 
 									],
 									
 		// Theme Options			   
